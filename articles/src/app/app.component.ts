4698bc5cde115ff50db40f4bfeda5c2b8a9be486
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-05-19 15:52:58 -0300

    datos adicionales

--- articles/src/app/app.component.ts
+++ articles/src/app/app.component.ts
@@ -6,77 +6,101 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'articles';
-
-  art = {
-    codigo: 0,
-    descripcion: "",
-    precio: 0
+  
+  art={
+    codigo:0 ,
+    descripcion:"",
+    precio:0
   }
-  articulos = [
-    { codigo: 0, descripcion: "alimento1", precio: 10 },
-    { codigo: 1, descripcion: "alimento2", precio: 20 },
-    { codigo: 2, descripcion: "alimento3", precio: 30 },
-    { codigo: 3, descripcion: "alimento4", precio: 40 },
-    { codigo: 4, descripcion: "alimento5", precio: 50 },
-    { codigo: 5, descripcion: "alimento6", precio: 60 },
-    { codigo: 6, descripcion: "alimento7", precio: 70 },
-    { codigo: 7, descripcion: "alimento8", precio: 80 },
-    { codigo: 8, descripcion: "alimento9", precio: 90 },
-  ];
 
-  // Usar operador ternario para devolver un valor booleano
+  articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20},
+              ];
+
   hayRegistros() {
-    return this.articulos.length > 0 ? true : false;
+    return this.articulos.length>0;              
   }
 
-  // Usar método find para buscar el objeto con el código dado
   borrar(codigo:number) {
-    let obj = this.articulos.find(obj => obj.codigo == codigo);
-    if (obj) {
-      // Usar método filter para devolver un nuevo array sin el objeto borrado
-      this.articulos = this.articulos.filter(obj => obj.codigo != codigo);
-    }
+    for(let x=0;x<this.articulos.length;x++)
+      if (this.articulos[x].codigo==codigo)
+      {
+        this.articulos.splice(x,1);
+        return;
+      }
   }
 
   agregar() {
-    // Usar operador ternario para mostrar un alerta o ejecutar una función
-    this.art.codigo == 0 ? alert('Debe ingresar un codigo de articulo distinto a cero') : this.agregarArticulo();
-    
-  }
-
-  // Crear una función aparte para agregar el artículo
-  agregarArticulo() {
-    // Usar método some para comprobar si existe algún objeto con el mismo código
-    let existe = this.articulos.some(obj => obj.codigo == this.art.codigo);
-    // Usar operador ternario para mostrar un alerta o agregar el objeto al array
-    existe ? alert('ya existe un articulo con ese mismo nombre') : this.articulos.push(this.art);
-    // Reiniciar los valores del objeto art
-    this.art = {
-      codigo: 0,
-      descripcion: "",
-      precio: 0
+    if (this.art.codigo==0) {
+      alert('Debe ingresar un código de articulo distinto a cero');
+      return;
     }
-    
+    for(let x=0;x<this.articulos.length;x++)
+    if (this.articulos[x].codigo==this.art.codigo)
+    {
+      alert('ya existe un articulo con dicho codigo');
+      return;
+    }        
+    this.articulos.push({codigo:this.art.codigo,
+                         descripcion:this.art.descripcion,
+                         precio:this.art.precio });
+    this.art.codigo=0;
+    this.art.descripcion="";	
+    this.art.precio=0;    
   }
 
+  seleccionar(art: { codigo: number; descripcion: string; precio: number; }) {
+    this.art.codigo=art.codigo;
+    this.art.descripcion=art.descripcion;
+    this.art.precio=art.precio;
+  }
+
+  modificar() {
+    for(let x=0;x<this.articulos.length;x++)
+      if (this.articulos[x].codigo==this.art.codigo)
+      {
+        this.articulos[x].descripcion=this.art.descripcion;
+        this.articulos[x].precio=this.art.precio;
+        return;
+      }        
+    alert('No existe el código de articulo ingresado');
+  }
+
+  borrarSeleccion() {
+    // Buscar el índice del artículo que se está modificando o seleccionando
+    let indice = this.articulos.findIndex(art => art.codigo == this.art.codigo);
+    // Si se encontró el índice, borrar el elemento del array
+    if (indice != -1) {
+      this.articulos.splice(indice, 1);
+      // Limpiar los campos de código, descripción y precio
+      this.art.codigo = 0;
+      this.art.descripcion = "";
+      this.art.precio = 0;
+    }
+  }
   
- seleccionar(art:{codigo:number;descripcion:string;precio:number;}) {
-   // Usar destructuración de objetos para asignar las propiedades del objeto art al objeto this.art
-   ({codigo:this.art.codigo,
-   descripcion:this.art.descripcion,
-   precio:this.art.precio} = art);
- }
- modificar() {
-   // Usar método find para buscar el objeto con el mismo código que el objeto art
-   let obj = this.articulos.find(obj => obj.codigo == this.art.codigo);
-   if (obj) {
-     // Usar destructuración de objetos para asignar las propiedades del objeto this.art al objeto encontrado
-     ({descripcion:obj.descripcion,
-     precio:obj.precio} = this.art);
-   } else {
-     alert('No existe el codigo del articculo ingresado')
-   }
-   
- }
+  // Variable auxiliar para guardar el estado anterior
+artAnterior = {
+  codigo: 0,
+  descripcion: "",
+  precio: 0
+}
+
+// Método para guardar el estado anterior antes de crear un nuevo artículo
+guardarAnterior() {
+  this.artAnterior.codigo = this.art.codigo;
+  this.artAnterior.descripcion = this.art.descripcion;
+  this.artAnterior.precio = this.art.precio;
+}
+
+// Método para restaurar el estado anterior cuando se presiona el botón deshacer
+deshacer() {
+  this.art.codigo = this.artAnterior.codigo;
+  this.art.descripcion = this.artAnterior.descripcion;
+  this.art.precio = this.artAnterior.precio;
+}
+
 }
